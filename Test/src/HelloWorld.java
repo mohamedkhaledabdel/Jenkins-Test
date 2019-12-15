e4769e75f5964d9c36443b2a813e508cd053fcdf
Mohamed Khaled Abdelmeguid <mohamed.kh.abdelmeguid@gmail.com>
2019-12-15 11:58:59 +0200

    Update HelloWorld.java

--- Test/src/HelloWorld.java
+++ Test/src/HelloWorld.java
@@ -2,11 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("exit");
-=======
 		System.out.println("This is triggered from jenkins after commiting to Git");
->>>>>>> c971d74401ed38925b962529c6cf425588c78ad4
 	}
 }
 
